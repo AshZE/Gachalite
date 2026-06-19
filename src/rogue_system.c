c9e0e463084310ac42285742f552757bbf677f6e
AshZE <eecksdeeoof@gmail.com>
2026-06-19 16:59:37 -0500

    Stray C fix

--- src/rogue_system.c
+++ src/rogue_system.c
@@ -27,7 +27,7 @@ static const struct UpgradeEssenceTableEntry sUpgradeTable[] = {
             .newType2 = TYPE_GROUND,
             .statBuffs = { [STAT_ATK] = 20, [STAT_SPEED] = 15 },
             .newAbilities = { ABILITY_SAND_FORCE, ABILITY_SAND_RUSH, ABILITY_STEADFAST },
-    c        .moveUnlocks = { MOVE_SACRED_FIRE, MOVE_NONE },
+            .moveUnlocks = { MOVE_SACRED_FIRE, MOVE_NONE },
         }
     },
     {
